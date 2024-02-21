984058d61f2dcf93df04b8320b6d502c75019dc6
HumbleLu <chienlu620@gmail.com>
2024-02-21 18:24:42 +0200

    add something to unif_act_fun.cpp

--- unif_act_fun.cpp
+++ unif_act_fun.cpp
@@ -2,19 +2,28 @@
 
 using namespace sycl;
 
-double ml_approx(double z, double ALPHA, double BETA, size_t N){
+double unif_act_approx(
+    double f_x, double g_x, 
+    double ALPHA_1, double BETA_1, 
+    double ALPHA_2, double BETA_2,
+    size_t N){
+    
     // A function that approximates Mittag-Leffler function
-    // input: z
+    // input: x
     // parameters: 
-    //     ALPHA;
-    //     BETA;
+    //     ALPHA_1, BETA_1
+    //     ALPHA_2, BETA_2
     //     N: controls the precision of the approximation;
 
-    //initilize the vector
-    std::vector<double> vector(N, 1.0);
+    //initilize the vector for the numerator
+    std::vector<double> num_vector(N, 1.0);
+    
+    //initilize the vector for the denominator
+    std::vector<double> den_vector(N, 1.0);
 
-     // create a buffer
-    buffer<double, 1> vector_buffer(vector.data(), range<1>(N));
+     // create buffers accordingly
+    buffer<double, 1> num_vector_buffer(num_vector.data(), range<1>(N));
+    buffer<double, 1> den_vector_buffer(den_vector.data(), range<1>(N));
 
     //# Create a device queue with device selector
     queue q(gpu_selector_v);
@@ -25,41 +34,36 @@ double ml_approx(double z, double ALPHA, double BETA, size_t N){
     q.submit([&](handler &h) {
 
       //# Create accessor for vector_buffer
-      accessor vector_accessor (vector_buffer, h);
+      accessor num_vector_accessor (num_vector_buffer, h);
+      accessor den_vector_accessor (den_vector_buffer, h);
 
       h.parallel_for(range<1>(N), [=](id<1> k) {
-         vector_accessor[k] =  sycl::pow(z, double(k)) / sycl::tgamma(ALPHA * double(k) + BETA);
+         num_vector_accessor[k] =  sycl::pow(f_x, double(k)) / sycl::tgamma(ALPHA_1 * double(k) + BETA_1);
+         den_vector_accessor[k] =  sycl::pow(g_x, double(k)) / sycl::tgamma(ALPHA_2 * double(k) + BETA_2);
       });
     }).wait();
 
     //# Create a host accessor to copy data from device to host
     {
-        host_accessor h_a(vector_buffer, read_only);
-        for(int i = 0; i < N; i++) std::cout << vector[i] << " ";
+        host_accessor h_a(num_vector_buffer, read_only);
+        for(int i = 0; i < N; i++) std::cout << num_vector[i] << " ";
+        std::cout << "" << std::endl;
+        
+        host_accessor h_b(den_vector_buffer, read_only);
+        for(int i = 0; i < N; i++) std::cout << den_vector[i] << " ";
+        std::cout << "" << std::endl;
     }
 
     std::cout << "now reducing.." << std::endl;
 
-    // Initilize sum
-    double sum = 0.0;
-    {
-        // Create a buffer for sum to get the reduction results
-        buffer<double> sum_buf{&sum, 1};
-
-        // Submit a SYCL kernel into a queue
-        q.submit([&](handler &cgh) {
-          // We can use built-in reduction primitive
-          auto sum_reduction = reduction(sum_buf, cgh, plus<double>());
-
-          //# Create accessor for vector_buffer
-          accessor vector_accessor (vector_buffer, cgh);
-
-          // A reference to the reducer is passed to the lambda
-          cgh.parallel_for(range<1>{N}, sum_reduction,
-                          [=](id<1> idx, auto &reducer) { reducer.combine(vector_accessor[idx]); });
-        }).wait();
-    }
-
     // return the sum
+    double sum = 0.0;
     return sum;
+}
+
+int main() {
+    double a = unif_act_approx(1.0, 2.0, 3.0, 2.0, 1.0, 1.4, 10);
+    std::cout << a << std::endl;
+
+    return 0;
 }
