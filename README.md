b84ac9b3e385b3fbf2a79aacf2bcd5cad7fb9420
HumbleLu <chienlu620@gmail.com>
2024-02-21 14:58:29 +0200

    add mlf_approx.cpp

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # sycl_fun
 A repository to dump my sycl-implemented functions
 
+- mlf_approx.cpp: a sycl implementation for the approximating the Mittag-Leffler function (MLF)
 - unif_act_fun.cpp: a sycl implementation for the unified activate function proposed in [1]
 
 [1] Mostafanejad, M. (2023). Unification of popular artificial neural network activation functions. arXiv preprint arXiv:2302.11007.
---
+++ mlf_approx.cpp
@@ -0,0 +1,65 @@
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
+
+double mlf_approx(double z, double ALPHA, double BETA, size_t N){
+    // A function that approximates Mittag-Leffler function
+    // input: z
+    // parameters: 
+    //     ALPHA;
+    //     BETA;
+    //     N: controls the precision of the approximation;
+
+    //initilize the vector
+    std::vector<double> vector(N, 1.0);
+
+     // create a buffer
+    buffer<double, 1> vector_buffer(vector.data(), range<1>(N));
+
+    //# Create a device queue with device selector
+    queue q(gpu_selector_v);
+
+    //# Print the device name
+    std::cout << "Device: " << q.get_device().get_info<info::device::name>() << "\n";
+
+    q.submit([&](handler &h) {
+
+      //# Create accessor for vector_buffer
+      accessor vector_accessor (vector_buffer, h);
+
+      h.parallel_for(range<1>(N), [=](id<1> k) {
+         vector_accessor[k] =  sycl::pow(z, double(k)) / sycl::tgamma(ALPHA * double(k) + BETA);
+      });
+    }).wait();
+
+    //# Create a host accessor to copy data from device to host
+    {
+        host_accessor h_a(vector_buffer, read_only);
+        for(int i = 0; i < N; i++) std::cout << vector[i] << " ";
+    }
+
+    std::cout << "now reducing.." << std::endl;
+
+    // Initilize sum
+    double sum = 0.0;
+    {
+        // Create a buffer for sum to get the reduction results
+        buffer<double> sum_buf{&sum, 1};
+
+        // Submit a SYCL kernel into a queue
+        q.submit([&](handler &cgh) {
+          // We can use built-in reduction primitive
+          auto sum_reduction = reduction(sum_buf, cgh, plus<double>());
+
+          //# Create accessor for vector_buffer
+          accessor vector_accessor (vector_buffer, cgh);
+
+          // A reference to the reducer is passed to the lambda
+          cgh.parallel_for(range<1>{N}, sum_reduction,
+                          [=](id<1> idx, auto &reducer) { reducer.combine(vector_accessor[idx]); });
+        }).wait();
+    }
+
+    // return the sum
+    return sum;
+}
